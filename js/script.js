c637d9bda0a7c15aebbc0682b20de9d0a5469b6c
moodang <yssmi@naver.com>
2023-12-21 12:59:19 +0900

    이벤트 활용

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 $(function () {
+  // 대상을 변수에 저장
+  const $btnTop = $('.btn-top');
+  const $header = $('#header');
+
   // fullpage 초기화
   $('#fullpage').fullpage({
     // 1. 앵커 설정
@@ -17,10 +21,39 @@ $(function () {
 
     // 영역 콘텐츠의 세로 정렬
     verticalCentered: false,
+
+    // 영역 로딩이 되고 나서
+    afterLoad: function (anchorLink, index) {
+      var loadedSection = $(this);
+      console.log('로딩된 후 :' + anchorLink, index, loadedSection);
+
+      if (anchorLink === 'sec4') {
+        $btnTop.fadeIn();
+      }
+    },
+
+    // 영역에서 떠날 때
+    onLeave: function (index, nextIndex, direction) {
+      var leavingSection = $(this);
+      console.log('영역에서 떠날 때 :' + index, nextIndex, direction, leavingSection);
+
+      if (index === 4 && direction === 'up') {
+        $btnTop.fadeOut();
+      }
+
+      if (direction === 'down') {
+        $header.addClass('hide');
+      } else {
+        $header.removeClass('hide');
+      }
+    },
   });
 
+  // 처음에는 숨기고
+  $btnTop.hide();
+
   // btn-top 버튼을 클릭했을 때
-  $('.btn-top').on('click', function (e) {
+  $btnTop.on('click', function (e) {
     e.preventDefault();
 
     // fullpage 메서드 : 원하는 영역 이동
