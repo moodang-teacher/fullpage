3e1d1ca2ce117e728a5db6e77d10a6e69c1b73f5
moodang <yssmi@naver.com>
2024-01-05 17:46:43 +0900

    지민씨 옵션

--- js/script.js
+++ js/script.js
@@ -3,64 +3,87 @@ $(function () {
   const $btnTop = $('.btn-top');
   const $header = $('#header');
 
-  // fullpage 초기화
-  $('#fullpage').fullpage({
-    // 1. 앵커 설정
-    anchors: ['sec1', 'sec2', 'sec3', 'sec4'],
-    // navigation: true,
+  $(window).on('scroll touchmove wheel', (e) => {
+    if (e.originalEvent.deltaY > 0) {
+      e.preventDefault();
+      e.stopPropagation();
+      return false;
+    }
+  });
 
-    // 2. menu적용(인디케이터 커스텀)
-    menu: '#myMenu',
-
-    // 3. 필요 옵션 적용
-    // 스크롤 속도 : 기본값 700
-    scrollingSpeed: 1400,
-
-    // 고정시킬 요소
-    fixedElements: '#header',
-
-    // 영역 콘텐츠의 세로 정렬
-    verticalCentered: false,
-
-    // 스크롤바 생성되게
-    scrollBar: true,
-
-    // 큰 영역으로 이동할 때 상단에 철컥 붙게
-    bigSectionsDestination: 'top',
-
-    // 영역 로딩이 되고 나서
-    afterLoad: function (anchorLink, index) {
-      var loadedSection = $(this);
-      console.log('로딩된 후 :' + anchorLink, index, loadedSection);
-
-      if (anchorLink === 'sec4') {
-        $btnTop.fadeIn();
-      }
-
-      // 두번째 영역에서는 자동 스크롤 취소
-      if (anchorLink === 'sec2') {
-        $.fn.fullpage.setAutoScrolling(false);
-      } else {
-        $.fn.fullpage.setAutoScrolling(true);
-      }
-    },
-
-    // 영역에서 떠날 때
-    onLeave: function (index, nextIndex, direction) {
-      var leavingSection = $(this);
-      console.log('영역에서 떠날 때 :' + index, nextIndex, direction, leavingSection);
-
-      if (index === 4 && direction === 'up') {
-        $btnTop.fadeOut();
-      }
-
-      if (direction === 'down') {
-        $header.addClass('hide');
-      } else {
-        $header.removeClass('hide');
-      }
+  // GSAP : intro animation
+  gsap.to('.intro', {
+    yPercent: -100,
+    duration: 1,
+    ease: 'power4.inOut',
+    delay: 3,
+    onComplete: () => {
+      $('.intro').css('display', 'none');
+      fullpageInit();
+      $(window).off('scroll touchmove wheel');
     },
   });
+
+  // fullpage 초기화
+  function fullpageInit() {
+    $('#fullpage').fullpage({
+      // 1. 앵커 설정
+      anchors: ['sec1', 'sec2', 'sec3', 'sec4'],
+      // navigation: true,
+
+      // 2. menu적용(인디케이터 커스텀)
+      menu: '#myMenu',
+
+      // 3. 필요 옵션 적용
+      // 스크롤 속도 : 기본값 700
+      scrollingSpeed: 1400,
+
+      // 고정시킬 요소
+      fixedElements: '#header',
+
+      // 영역 콘텐츠의 세로 정렬
+      verticalCentered: false,
+
+      // 스크롤바 생성되게
+      scrollBar: true,
+
+      // 큰 영역으로 이동할 때 상단에 철컥 붙게
+      bigSectionsDestination: 'top',
+
+      // 영역 로딩이 되고 나서
+      afterLoad: function (anchorLink, index) {
+        var loadedSection = $(this);
+        console.log('로딩된 후 :' + anchorLink, index, loadedSection);
+
+        if (anchorLink === 'sec4') {
+          $btnTop.fadeIn();
+        }
+
+        // 두번째 영역에서는 자동 스크롤 취소
+        if (anchorLink === 'sec2') {
+          $.fn.fullpage.setAutoScrolling(false);
+        } else {
+          $.fn.fullpage.setAutoScrolling(true);
+        }
+      },
+
+      // 영역에서 떠날 때
+      onLeave: function (index, nextIndex, direction) {
+        var leavingSection = $(this);
+        console.log('영역에서 떠날 때 :' + index, nextIndex, direction, leavingSection);
+
+        if (index === 4 && direction === 'up') {
+          $btnTop.fadeOut();
+        }
+
+        if (direction === 'down') {
+          $header.addClass('hide');
+        } else {
+          $header.removeClass('hide');
+        }
+      },
+    });
+  }
 
   // 처음에는 숨기고
   $btnTop.hide();
